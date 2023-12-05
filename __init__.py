870cc015f472a543fef43a8f62ca0bcdf03c3949
Google.3616 <joshua.lander@tallulahfalls.org>
2023-12-05 08:31:25 -0500

    Update __init__.py docstring

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-"""A module for handling EBF, also known as Extended Brainfuck. With graphics handling, psuedo-random functionality, and more, it still keeps this simple language to less than 15 characters."""
+"""A module for handling EBF, also known as Extended Brainf\*\*k. With graphics handling, psuedo-random functionality, and more, it still keeps this simple language to less than 15 characters."""
 from ExtendedBF.extendedbf.__functions import *
